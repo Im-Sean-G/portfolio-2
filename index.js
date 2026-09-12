3fcdaed243ab792b3c71c836f83d196eb6067ea2
Im-Sean-G <seanrgallagher88@gmail.com>
2026-09-12 14:26:40 -0400

    email form code written

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 // Open Modal 
 
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
 
 
 // Email Contact
 
 // template_2w0sbig
-// service_p1kq892
+// service_1eyogya
 // OSsOV_yTW8hBeFR22
 
 function contact(event) {
   event.preventDefault();
-  const loading = document.querySelector('.modal__overlay--loading')
-  const success = document.querySelector('.modal__overlay--success')
-  loading.classList += " modal__overlay--visible"
-
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible";
   emailjs
-    sendForm(
-      'service_p1kq892'
-      'service_p1kq892'
+    .sendForm(
+      'service_1eyogya',
+      'template_2w0sbig',
       event.target,
       'OSsOV_yTW8hBeFR22'
-    ).then(() => {
-      loading.classList.remove("modal__overlay--visible")
-      success.classList += " modal__overlay--visible"
-    }).catch(() => {
-      loading.classList.remove("modal__overlay--visible")
-      alert(
-        "The email service is temporarily unavailable. Please contact me directly at seanrgallagher88@gmail.com"
-      )
-    })
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";   
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+      "The email service is temporarily unavailable. Please contact me directly at seanrgallagher88@gmail.com"
+    );
+  })
 }
